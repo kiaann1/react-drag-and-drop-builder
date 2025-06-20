6386b1d371162eb5202981d3ae0545fd26467753
kiaann1 <kian.winwood1@gmail.com>
2025-06-20 10:00:02 +0100

    added analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FormBuilder from "./components/FormBuilder";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <ErrorBoundary>
       <FormBuilder />
+      <Analytics />
     </ErrorBoundary>
   );
 }
