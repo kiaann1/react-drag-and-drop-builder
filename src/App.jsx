ff1fb30e237c07191ebcbb034546d3586d66c90a
kiaann1 <kian.winwood1@gmail.com>
2025-06-20 10:06:47 +0100

    page insights vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import FormBuilder from "./components/FormBuilder";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <ErrorBoundary>
       <FormBuilder />
       <Analytics />
+      <SpeedInsights />
     </ErrorBoundary>
   );
 }
