3a6a06f4485a3dcaa816a1a90cc06cc81cece8f6
kiaann1 <kian.winwood1@gmail.com>
2025-06-20 10:02:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormBuilder from "./components/FormBuilder";
 import ErrorBoundary from "./components/ErrorBoundary";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
